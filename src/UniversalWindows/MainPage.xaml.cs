615a0702a73237a0b9d27ecc24ee6962f311ff47
Stefan Over <herdosratos@googlemail.com>
2018-06-12 22:15:26 +0200

    basic UWP implementation

---
+++ src/UniversalWindows/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Herdo.StrongTypes.UniversalWindows
+{
+    using Windows.UI.Xaml;
+
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var ci = new CustomerInfo();
+            ContentTextBlock.Text = ci.GetMembershipDetails();
+        }
+    }
+}
